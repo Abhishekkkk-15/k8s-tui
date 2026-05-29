b3db39becfe1012874ba1d02e9fc8944a879235e
Abhishek jangid <164181412+Abhishekkkk-15@users.noreply.github.com>
2026-05-29 22:01:06 +0530

    feat: simple code for listing pods in default namespace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use kube::{Client, Api};
+use k8s_openapi::api::core::v1::Pod;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Infers configuration from ~/.kube/config or in-cluster environment
+    let client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::default_namespaced(client);
+
+    for p in pods.list(&Default::default()).await? {
+        println!("Found Pod: {}", p.metadata.name.unwrap());
+    }
+    Ok(())
 }
